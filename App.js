0605d337928bd8aea79e5082123aab187511189a
rojaece0711 <rojaece0711@gmail.com>
2024-06-02 21:22:11 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import LoginForm from './LoginForm/LoginForm';
+
+function App() {
+  return (
+    <div >
+        <LoginForm/>
+    </div>
+  );
+}
+
+export default App;
+
